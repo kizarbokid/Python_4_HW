a544bc67eea7ac2e0cc6b91de483e011e77418d9
YakovkinVD <dikobrazzzik709@gmail.com>
2023-01-05 00:34:45 +0900

    import added

--- HW_4.py
+++ HW_4.py
@@ -34,18 +34,63 @@ def toCreatePolynomialStr(polynomial_dict):
             polynomial_str += str(value)+'*x^'+str(key)+'+'
 
 
-def toWriteSomePolyInTXT(value: str, filename):
+def toWriteSomeStrInTXT(value: str, filename):
     with open(filename, "w") as file:
         file.write(value)
     return print(f'Запись файла {filename} завершена')
 
 
+def toReadSomeStrFromTXT(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+    print(f'Импорт из файла {filename} завершен')
+    return content
+
+
+def toGetValuesFromString(string):
+    values_dict = dict()
+    string = string.replace('-', '+-')
+    values_list = [string for string in string.split('+')]
+    if values_list[0] == '':  # если первый многочлен <0 то первый элемент пустая строка, уберем его
+        del values_list[0]
+    # определить степень многочлена
+    length = int(values_list[0][values_list[0].find('^')+1:])
+    for i in range(length, 0, -1):
+        values_dict[i] = 0
+    # записать свободный член
+    if '*' not in values_list[-1] and ('x' not in values_list[-1]):
+        values_dict[0] = int(values_list[-1])
+        values_list.pop()
+    else:
+        values_dict[0] = 0
+    # записать член в степени 1, если такой существует
+    for value in values_list:
+        if ('x' in value) and ('^' not in value):
+            if len(value) > 1:
+                values_dict[1] = int(value[:value.find('*')])
+                values_list.pop()
+            else:
+                values_dict[1] = 1
+    # записать в словарь все остальные члены
+    for value in values_list:
+        if '*' in value:
+            values_dict[int(value[value.find('^')+1:])] = int(value[:value.find('*')])
+        else:  # если нет знака умножения, значит коэффициент=1
+            values_dict[int(value[value.find('^')+1:])] = 1
+
+    return values_dict
+
+
 low = -100
-high = 101
+high = 100 + 1
 k = int(input('Введите степень многочлена k: '))
 my_dict = toCreateKoefForPolynomial(k, low, high)
 my_str = toCreatePolynomialStr(my_dict)
-toWriteSomePolyInTXT(my_str, 'polynomial1.txt')
+toWriteSomeStrInTXT(my_str, 'polynomial1.txt')
 my_dict = toCreateKoefForPolynomial(k, low, high)
 my_str = toCreatePolynomialStr(my_dict)
-toWriteSomePolyInTXT(my_str, 'polynomial2.txt')
+toWriteSomeStrInTXT(my_str, 'polynomial2.txt')
+import_str1 = toReadSomeStrFromTXT('polynomial1.txt')
+import_str2 = toReadSomeStrFromTXT('polynomial2.txt')
+import_dict1=toGetValuesFromString(import_str1)
+import_dict2=toGetValuesFromString(import_str2)
